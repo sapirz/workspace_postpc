df04ebe6de1a47fc76e30dc8a3737b8ce8b8d731
sapirz <sapir.zan@mail.huji.ac.il>
2013-03-20 14:18:47 +0200

    ex3 - start changes

--- TodoListManager/src/il/ac/huji/todolist/Task.java
+++ TodoListManager/src/il/ac/huji/todolist/Task.java
@@ -1,11 +1,16 @@
 package il.ac.huji.todolist;
 
+import java.util.Date;
+
 public class Task {
 	
 	public String taskName;
+	public Date dueDate;
 	
-	public Task(String name) {
+	public Task(String name, Date d/*TODO - complete!*/) {
 		this.taskName = name;
+		this.dueDate = d;
+		
 	}
 	
 	public String toString() {
--- TodoListManager/src/il/ac/huji/todolist/TodoListManagerActivity.java
+++ TodoListManager/src/il/ac/huji/todolist/TodoListManagerActivity.java
@@ -1,15 +1,21 @@
 package il.ac.huji.todolist;
 
+
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 
 public class TodoListManagerActivity extends Activity {
 
@@ -32,6 +38,8 @@ public class TodoListManagerActivity extends Activity {
 
 		listTasks.setAdapter(adapter);
 
+		registerForContextMenu(listTasks);
+
 	}
 
 	@Override
@@ -40,22 +48,51 @@ public class TodoListManagerActivity extends Activity {
 		return true;
 	}
 
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		menu.setHeaderTitle("This is my title");//TODO - complete
+		getMenuInflater().inflate(R.menu.contextmenu, menu);//TODO????
+		//set title
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		int selectedItemIndex = info.position;
+		switch (item.getItemId()){
+		case R.id.menuItemDelete:
+			adapter.remove(adapter.getItem(selectedItemIndex));
+			break;
+		case R.id.menuItemCall:
+			//TODO - call!!
+			break;
+		}
+
+		return true;
+	}
+
+
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case R.id.menuItemAdd:
-			EditText newTask = (EditText) findViewById(R.id.edtNewItem);
-			if (newTask.getText().length() != 0){
-				adapter.add(new Task(newTask.getText().toString()));//get task name from edtNewItem	
-			}
+			//TODO - should open an add dialog...
+			//EditText newTask = (EditText) findViewById(R.id.edtNewItem);
+			String newTask = "temp";
+			//if (newTask.getText().length() != 0){
+				adapter.add(new Task(newTask/*.getText().toString()*/, new Date()));//get task name from edtNewItem	
+			//}
 			break;
-		case R.id.menuItemDelete:
+		/*case R.id.menuItemDelete:
 			//get selected item and remove it from the list
 			Task t = (Task) listTasks.getSelectedItem();
 			if (t!=null) {
 				adapter.remove(t);
 			}
-			break;
+			break; TODO - move to other place...*/
 		}
 		return true;
 	}
